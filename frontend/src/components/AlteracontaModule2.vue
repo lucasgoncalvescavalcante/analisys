4c44556efa3159057e226f5913544b647c883c6a
lucasgoncalvescavalcante <lucasgoncalvescavalcante@gmail.com>
2023-08-02 17:12:32 -0400

    AlterarConta2 c/ datatables

--- frontend/src/components/AlteracontaModule2.vue
+++ frontend/src/components/AlteracontaModule2.vue
@@ -1,23 +1,37 @@
 <template>
-  <div>
-    <div>
-  <label for="userStatusFilter">Filtrar por status:</label>
-  <select id="userStatusFilter">
-    <option value="">Todos</option>
-    <option value="ativo">Ativos</option>
-    <option value="inativo">Inativos</option>
-  </select>
-</div>
-    <table id="listusers" class="display" style="width:100%">
+  <div class="container mt-4">
+    <form>
+      <div class="form-group row">
+        <label for="contas-input" class="col-sm-2 col-form-label">Contas a Pagar:</label>
+        <div class="col-sm-4 mb-3">
+          <input type="text" class="form-control" id="contas-input" v-model="contasid">
+        </div>
+        <div class="col-sm-2">
+          <button type="button" class="btn btn-primary" @click="getContas">Consultar</button>
+        </div>
+      </div>
+    </form>
+
+    <table id="contasTable" class="display" style="width:100%">
       <thead>
         <tr>
-          <th>Username</th>
-          <th>Name</th>
-          <th>Status</th>
-          <th>Ação</th>
+          <th>ID</th>
+          <th>Conta Contábil</th>
+          <th>Período Início</th>
+          <th>Período Fim</th>
+          <th>Lote</th>
         </tr>
       </thead>
+      <tbody>
+      </tbody>
     </table>
+    
+    <div class="form-group row mb-3">
+      <div class="col-sm-2">
+        <button type="button" class="btn btn-primary" @click="getSelectedIds()">Adicionar Linha</button>
+        <button type="button" class="btn btn-primary" @click="selectAllRows()">selecionar todos</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,44 +39,76 @@
 import $ from 'jquery';
 import 'datatables.net';
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
-import language from 'datatables.net-plugins/i18n/pt-BR.mjs';
+import 'datatables.net-select';
+import 'datatables.net-select-bs5/css/select.bootstrap5.min.css';
+import axios from 'axios';
 
 export default {
-  mounted() {
-    this.initializeDataTable();
+  data() {
+    return {
+      contasid: '',
+      dataTable: null,
+      contasapagar: {}
+    };
   },
   methods: {
-    initializeDataTable() {
-      $('#listusers').DataTable({
-        serverSide: true,
-        processing: true,
-        language,
-        paging: true,
-        searching: true, // Habilitar busca
-        ordering: true,
-        searchDelay: 10000, // Atraso de busca de 500ms (opcional, ajuste conforme necessário)
-        ajax: {
-          url: 'http://localhost:3000/api/v1/getdata',
-          type: 'GET',
-        },
-        columns: [
-          { data: 'username' }, // Coluna para o campo "username"
-          { data: 'name' }, // Coluna para o campo "name"
-          {  data: 'isactive',
-            render: function (data) {
-              if (data === 0) {
-                return '<span class="badge bg-danger">Inativo</span>';
-              } else if (data === 1) {
-                return '<span class="badge bg-success">Ativo</span>';
-              } else {
-                return '';
-              }
-            }
-          },      
-          { data: 'editButton'},
-        ],
-      });
+    async getContas() {
+      try {
+        const response = await axios.get(`http://localhost:3000/api/v1/getContas/${this.contasid}`);
+        const data = response.data;
+
+        Limpa o corpo da tabela antes de adicionar novas linhas
+        this.dataTable.clear();
+
+        // Adiciona novas linhas para cada item no array de dados
+        data.forEach(item => {
+          this.dataTable.row.add(item);
+        });
+
+        // Desenha a tabela com as novas linhas
+        this.dataTable.draw();
+
+        // Adiciona uma nova linha automaticamente
+       // this.addNewRow();
+        //const selectedids = this.getSelectedIds();
+       // console.log('aqui estao os ids', selectedids);
+      } catch (err) {
+        console.error(err);
+      }
     },
+    getSelectedIds() {
+      const selectedRowsData = this.dataTable.rows({ selected: true }).data().toArray();
+      const selectedIds = selectedRowsData.map(row => row.id); // Verifica se o ID está sendo corretamente extraído
+      console.log('Aqui estão os IDs selecionados:', selectedIds);
+      return selectedIds;
+    },
+      selectAllRows() {
+    const anySelected = this.dataTable.rows({ selected: true }).any();
+
+    if (anySelected) {
+      // Se pelo menos uma linha estiver selecionada, deseleciona todas
+      this.dataTable.rows().deselect();
+    } else {
+      // Se nenhuma linha estiver selecionada, seleciona todas
+      this.dataTable.rows().select();
+    }
+  }
   },
+  mounted() {
+    this.dataTable = $('#contasTable').DataTable({
+      searching: false,
+      paging: false,
+      select: true,
+      className: 'select-checkbox',
+      columns: [
+        { data: 'id' },
+        { data: 'contacontabil' },
+        { data: 'periodoInicio' },
+        { data: 'periodoFim' },
+        { data: 'lote' }
+      ]
+    });
+  }
 };
+
 </script>
